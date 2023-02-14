a11e1a4335b8b77cbfee3555ff13da5c4f5b4948
Amasukura <makihiro.2b30@me.com>
2023-02-14 10:26:30 +0900

    [Add] index

--- dB_S_11.py
+++ dB_S_11.py
@@ -1,14 +1,22 @@
 import csv
 import matplotlib.pyplot as plt
+import os
 
 freq = []
-S11 = []
+S11_dB = []
 
 freq_start = 125
-
 freq_end = 211
 
-result_path = "result/H-bend_dB.csv"
+result_dir = "result"
+filename = "H-bend_dB.csv"
+
+result_path = result_dir + "/" +filename
+
+if not os.path.exists(result_dir):
+    print("Result directry does not exist.")
+    os.makedirs(result_dir)
+    quit()
 
 with open(result_path) as f:
     reader = csv.reader(f)
@@ -19,7 +27,7 @@ with open(result_path) as f:
         else:
             if freq_start <= float(row[1]) <= freq_end:
                 freq.append(float(row[1]))
-                S11.append(float(row[2]))
+                S11_dB.append(float(row[2]))
             else:
                 continue
 
@@ -48,43 +56,48 @@ ax = fig.add_subplot(111)
 ax.axhspan(-100, -15, color = "deepskyblue", alpha = 0.5) #y方向塗りつぶし
 ax.axhspan(-100, -20, color = "darkcyan", alpha = 0.3)
 
-ax.plot(freq,S11,"k-",label="$S_{11}$")
+ax.plot(freq,S11_dB,"k-",label="$S_{11}$")
 
 ax.set_xlim(freq_start, freq_end)
 ax.set_ylim(-55,0)
 
 h1, l1 = ax.get_legend_handles_labels()
 ax.legend(h1, l1)
-#ax.legend(h1, l1)
+
+ax.set_xlabel("Freqency [GHz]")
+ax.set_ylabel("Magnitude $S_{11}$ [dB]")
+
+fig_dir = "Figs"
+if not os.path.exists(fig_dir):
+    print("Fig directry does not exist.")
+    os.makedirs(fig_dir)
+
+plt.savefig(fig_dir+"/"+(result_path.replace(".csv","")).replace("result/","")+".png")
+plt.show()
 
 ###############################################################analysis#######################################################################
 
-print("Max S11:"+str(max(S11))+"[dB] "+"Freqency:"+str(freq[S11.index(max(S11))])+"[GHz]")
-print("min S11:"+str(min(S11))+"[dB] "+"Freqency:"+str(freq[S11.index(min(S11))])+"[GHz]")
+print("Max S11:"+str(max(S11_dB))+"[dB] "+"Freqency:"+str(freq[S11_dB.index(max(S11_dB))])+"[GHz]")
+print("min S11:"+str(min(S11_dB))+"[dB] "+"Freqency:"+str(freq[S11_dB.index(min(S11_dB))])+"[GHz]")
 
 freq_under_10 = []
 for i in range(len(freq)):
-    if S11[i] < -10:
-        freq_under_10.append(freq[S11.index(S11[i])])
+    if S11_dB[i] < -10:
+        freq_under_10.append(freq[S11_dB.index(S11_dB[i])])
 
 print("Percentage of -10 dB range:"+str((len(freq_under_10)/len(freq))*100)+" [%] between "+str(min(freq))+" [GHz] and "+str(max(freq))+" [GHz]")
 
 freq_under_15 = []
 for i in range(len(freq)):
-    if S11[i] < -15:
-        freq_under_15.append(freq[S11.index(S11[i])])
+    if S11_dB[i] < -15:
+        freq_under_15.append(freq[S11_dB.index(S11_dB[i])])
 
 print("Percentage of -15 dB range:"+str((len(freq_under_15)/len(freq))*100)+" [%] between "+str(min(freq))+" [GHz] and "+str(max(freq))+" [GHz]")
 
 freq_under_20 = []
 for i in range(len(freq)):
-    if S11[i] < -20:
-        freq_under_20.append(freq[S11.index(S11[i])])
+    if S11_dB[i] < -20:
+        freq_under_20.append(freq[S11_dB.index(S11_dB[i])])
 
 print("Percentage of -20 dB range:"+str((len(freq_under_20)/len(freq))*100)+" [%] between "+str(min(freq))+" [GHz] and "+str(max(freq))+" [GHz]")
 
-ax.set_xlabel("Freqency [GHz]")
-ax.set_ylabel("Magnitude $S_{11}$ [dB]")
-
-plt.savefig("Figs/"+(result_path.replace(".csv","")).replace("result/","")+".png")
-plt.show()
